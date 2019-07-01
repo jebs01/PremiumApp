b4cf10a1e8a1a03a5aa21b2db1faa6ee021c16b1
Jb <jebs01@users.noreply.github.com>
2019-07-02 08:22:17 +1000

    Update UserForCalculationDto.cs

--- PremiumApp.API/Dtos/UserForCalculationDto.cs
+++ PremiumApp.API/Dtos/UserForCalculationDto.cs
@@ -11,12 +11,16 @@ namespace PremiumApp.API.Dtos
         
         public string Age { get; set; }
 
+        [Required]
         public string DateofBirth { get; set; }
 
+        [Required]
         public string Occupation { get; set; }
 
+        [Required]
          public double SumInsured { get; set; }
 
+       [Required]
         public double Premium {get;set;}
     }
 }
