dbac3d2ea44be9a7fbc1216f0a5c9ffc9661af02
Jb <jebs01@users.noreply.github.com>
2019-07-02 08:04:09 +1000

    Create OccupationRating.cs

---
+++ PremiumApp.API/Data/OccupationRating.cs
@@ -0,0 +1,11 @@
+namespace PremiumApp.API.Data
+{
+    public static class OccupationRating
+    {
+            public const double NotSet = 0;
+            public const double Professional = 1.0;
+            public const double WhiteCollar = 1.25;
+             public const double LightManual = 1.5;
+            public const double HeavyManual = 1.75;
+    }
+}
