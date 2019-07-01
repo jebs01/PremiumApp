e023a181c3e91fa39c3ff583a9779557cfb0e0ad
Jb <jebs01@users.noreply.github.com>
2019-07-02 08:07:11 +1000

    Update auth.service.ts

--- PremiumApp-SPA/src/app/_services/auth.service.ts
+++ PremiumApp-SPA/src/app/_services/auth.service.ts
@@ -10,16 +10,7 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  login(model: any) {
-    return this.http.post(this.baseUrl + 'login', model).pipe(
-      map((response: any) => {
-        const user = response;
-        if (user) {
-          localStorage.setItem('token', user.token);
-        }
-      })
-    );
-  }
+ 
 
   calculate(model: any) {
     return this.http.post(this.baseUrl + 'calculate', model).pipe(
